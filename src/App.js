d6f419d498ea67c455da06ec11a4337bede4c75f
Antonio Mendoza <antoniomendoza05@gmail.com>
2021-04-24 01:01:27 -0700

    front end changes

--- src/App.js
+++ src/App.js
@@ -1,133 +1,123 @@
-import 'semantic-ui-css/semantic.min.css'
-import './App.css';
+import "semantic-ui-css/semantic.min.css";
+import "./App.css";
 
-import React, {Component} from "react"
-import Goal from "./Goal"
+import React, { Component } from "react";
+import Goal from "./Goal";
 
-import { Button, Icon } from 'semantic-ui-react'
+import { Button, Icon } from "semantic-ui-react";
 
-import IconButton from "./components/IconButton"
+import IconButton from "./components/IconButton";
 
-import 'semantic-ui-css/semantic.min.css'
+import "semantic-ui-css/semantic.min.css";
 import { Container, Header, List } from "semantic-ui-react";
 
-import pkg from 'semantic-ui-react/package.json'
-import CreateModal from "./components/CreateModal"
-import EditModal from "./components/EditModal"
-import LoginModal from "./components/LoginModal"
+import pkg from "semantic-ui-react/package.json";
+import CreateModal from "./components/CreateModal";
+import EditModal from "./components/EditModal";
+import LoginModal from "./components/LoginModal";
 
-
-
-
-let baseURL = ''
+let baseURL = "";
 // const p = (x) => {console.log(x)}
 
 // p(process.env.NODE_ENV)
 
-if (process.env.NODE_ENV === 'development') {
-  baseURL = 'http://localhost:3003'
+if (process.env.NODE_ENV === "development") {
+  baseURL = "http://localhost:3003";
 } else {
-  baseURL = 'your heroku backend url here'
+  baseURL = "https://pixel-progress-back-end.herokuapp.com";
 }
 // p(`current base URL: ${baseURL}`)
 
 export default class App extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
-      goals: [],
-    }
-    }
-
-    getGoals = () => {
-      fetch(baseURL + '/goals')
-        .then(res => {return res.json()
-        }).then(data => {
-        this.setState({
-          goals: data,
-        })
-      })
-
+      goals: []
+    };
   }
 
+  getGoals = () => {
+    fetch(baseURL + "/goals")
+      .then(res => {
+        return res.json();
+      })
+      .then(data => {
+        this.setState({
+          goals: data
+        });
+      });
+  };
 
-  deleteGoal = async (id) => {
-
-    const url = baseURL + "/goals/" + id
+  deleteGoal = async id => {
+    const url = baseURL + "/goals/" + id;
 
     try {
-      const response = await fetch(url, {method: "DELETE"})
+      const response = await fetch(url, { method: "DELETE" });
 
-      if (response.status === 200){
-
-        const index = this.state.goals.findIndex(goal => goal._id === id)
-        const copyGoals = [...this.state.goals]
-        copyGoals.splice(index, 1)
+      if (response.status === 200) {
+        const index = this.state.goals.findIndex(goal => goal._id === id);
+        const copyGoals = [...this.state.goals];
+        copyGoals.splice(index, 1);
         this.setState({
           goals: copyGoals
-        })
+        });
       }
-
+    } catch (error) {
+      console.log("error: ", error);
     }
-    catch(error){
-      console.log('error: ', error)
-    }
-  }
+  };
 
-  fillStep = (e) => {
-    e.preventDefault()
-    document.getElementById(`${e.target.id}`).classList.toggle('clickedStep')
-  }
+  fillStep = e => {
+    e.preventDefault();
+    document.getElementById(`${e.target.id}`).classList.toggle("clickedStep");
+  };
 
-  componentDidMount(){
-this.getGoals()
-}
+  componentDidMount() {
+    this.getGoals();
+  }
 
   render() {
     return (
-      <div className='App'>
+      <div className="App">
         <h1>Pixel Progress</h1>
         <LoginModal />
-        <br/><br/>
-        <CreateModal
-        getGoals={this.getGoals}
-        baseURL={baseURL}
-        />
+        <br />
+        <br />
+        <CreateModal getGoals={this.getGoals} baseURL={baseURL} />
 
         {this.state.goals.map(goal => {
           return (
             <>
-              <h3>{goal.name} &#160;&#160;
-              <EditModal
-                className="IconButton"
-                getGoals={this.getGoals}
-                baseURL={baseURL}
-                goal={goal}
-                goals={this.state.goals}
+              <h3>
+                {goal.name} &#160;&#160;
+                <EditModal
+                  className="IconButton"
+                  getGoals={this.getGoals}
+                  baseURL={baseURL}
+                  goal={goal}
+                  goals={this.state.goals}
                 />
-
-              <IconButton
-                className="IconButton"
-                icon={'trash alternate'}
-                actionFunction={() => {this.deleteGoal(goal._id)}}/>
+                <IconButton
+                  className="IconButton"
+                  icon={"trash alternate"}
+                  actionFunction={() => {
+                    this.deleteGoal(goal._id);
+                  }}
+                />
               </h3>
 
-
               <Goal
-              key={`${goal.name}-goal`}
-              name={goal.name}
-              sessions={goal.days}
-              steps={goal.categories}
-              reps={goal.week_start_dates}
-              fillStep={this.fillStep}
+                key={`${goal.name}-goal`}
+                name={goal.name}
+                sessions={goal.days}
+                steps={goal.categories}
+                reps={goal.week_start_dates}
+                fillStep={this.fillStep}
               />
             </>
-          )
+          );
         })}
-
-
-
       </div>
-    )
+    );
   }
 }
